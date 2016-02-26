b08f7ddb24bc59085b74cf42faf59e025a003db5
Dylan Jang <Dylan.Jang@nsuslab.com>
2016-02-26 10:56:40 +0900

    - update projectbuilder.cs.

--- Assets/Scripts/Editor/ProjectBuilder.cs
+++ Assets/Scripts/Editor/ProjectBuilder.cs
@@ -6,54 +6,76 @@ using UnityEditor;
 
 //namespace Assets.Scripts.Editor
 //{
-	class ProjectBuilder
+class ProjectBuilder
+{
+	static string[] SCENES = FindEnabledEditorScenes();
+	static string TARGET_DIR = "build";
+
+	[MenuItem("Custom/CI/Build Android")]
+	static void PerformAndroidBuild()
 	{
-		static string[] SCENES = FindEnabledEditorScenes();
-		static string TARGET_DIR = "build";
+		BuildOptions opt = BuildOptions.None;
 
-		[MenuItem("Custom/CI/Build iOS Debug")]
-		static void PerformiOSDebugBuild()
+		//char sep = Path.DirectorySeparatorChar;
+		////string BUILD_TARGET_PATH = Path.GetFullPath(".") + sep + TARGET_DIR + string.Format("/AndroidBuild_{0}.apk", PlayerSettings.buildVersion);
+		//string BUILD_TARGET_PATH = Path.GetFullPath(",") + sep + TARGET_DIR + "Poker.apk";
+
+		char sep = Path.DirectorySeparatorChar;
+		string buildDirectory = Path.GetFullPath(".") + sep + TARGET_DIR;
+		Directory.CreateDirectory(buildDirectory);
+
+		string BUILD_TARGET_PATH = buildDirectory + "/android";
+		Directory.CreateDirectory(BUILD_TARGET_PATH);
+
+		BUILD_TARGET_PATH += "/Poker.apk";
+
+		GenericBuild(SCENES, BUILD_TARGET_PATH, BuildTarget.Android, opt);
+	}
+
+	[MenuItem("Custom/CI/Build iOS Debug")]
+	static void PerformiOSDebugBuild()
+	{
+		BuildOptions opt = BuildOptions.SymlinkLibraries |
+						   BuildOptions.Development |
+						   BuildOptions.ConnectWithProfiler |
+						   BuildOptions.AllowDebugging |
+						   BuildOptions.Development |
+						   BuildOptions.AcceptExternalModificationsToPlayer;
+
+		PlayerSettings.iOS.sdkVersion = iOSSdkVersion.DeviceSDK;
+		PlayerSettings.iOS.targetOSVersion = iOSTargetOSVersion.iOS_4_3;
+		PlayerSettings.statusBarHidden = true;
+
+		char sep = Path.DirectorySeparatorChar;
+		string buildDirectory = Path.GetFullPath(".") + sep + TARGET_DIR;
+		Directory.CreateDirectory(buildDirectory);
+
+		string BUILD_TARGET_PATH = buildDirectory + "/ios";
+		Directory.CreateDirectory(BUILD_TARGET_PATH);
+
+		GenericBuild(SCENES, BUILD_TARGET_PATH, BuildTarget.iOS, opt);
+	}
+
+	private static string[] FindEnabledEditorScenes()
+	{
+		List<string> EditorScenes = new List<string>();
+		foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
 		{
-			BuildOptions opt = BuildOptions.SymlinkLibraries |
-							   BuildOptions.Development |
-							   BuildOptions.ConnectWithProfiler |
-							   BuildOptions.AllowDebugging |
-							   BuildOptions.Development;
-
-			PlayerSettings.iOS.sdkVersion = iOSSdkVersion.DeviceSDK;
-			PlayerSettings.iOS.targetOSVersion = iOSTargetOSVersion.iOS_4_3;
-			PlayerSettings.statusBarHidden = true;
-
-			char sep = Path.DirectorySeparatorChar;
-			string buildDirectory = Path.GetFullPath(".") + sep + TARGET_DIR;
-			Directory.CreateDirectory(buildDirectory);
-
-			string BUILD_TARGET_PATH = buildDirectory + "/ios";
-			Directory.CreateDirectory(BUILD_TARGET_PATH);
-
-			GenericBuild(SCENES, BUILD_TARGET_PATH, BuildTarget.iOS, opt);
+			if (!scene.enabled) continue;
+			EditorScenes.Add(scene.path);
 		}
 
-		private static string[] FindEnabledEditorScenes()
-		{
-			List<string> EditorScenes = new List<string>();
-			foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
-			{
-				if (!scene.enabled) continue;
-				EditorScenes.Add(scene.path);
-			}
+		return EditorScenes.ToArray();
+	}
 
-			return EditorScenes.ToArray();
-		}
-
-		static void GenericBuild(string[] scenes, string target_path, BuildTarget build_target, BuildOptions build_options)
+	static void GenericBuild(string[] scenes, string target_path, BuildTarget build_target, BuildOptions build_options)
+	{
+		EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+		string res = BuildPipeline.BuildPlayer(scenes, target_path, build_target, build_options);
+		if (res.Length > 0)
 		{
-			EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
-			string res = BuildPipeline.BuildPlayer(scenes, target_path, build_target, build_options);
-			if (res.Length > 0)
-			{
-				throw new Exception("BuildPlayer failure: " + res);
-			}
+			throw new Exception("BuildPlayer failure: " + res);
 		}
 	}
+}
 //}
